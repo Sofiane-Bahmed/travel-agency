7bff39f2dd428f6ec811fa69478a0f0fad3de851
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2026-01-21 20:51:57 +0100

    travel agency

---
+++ script.js
@@ -0,0 +1,133 @@
+
+// responsive navigatioin menu 
+
+const menuBtn = document.querySelector(".menue-btn");
+const navbar = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click",()=>{
+    menuBtn.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
+
+// video slider navigation 
+
+const btns = document.querySelectorAll(".nav-btn")
+const slides = document.querySelectorAll(".video-slide")
+const contents = document.querySelectorAll(".content")
+
+var slideNav = function(manual){
+    btns.forEach((btn)=>{
+        btn.classList.remove("active") 
+    });
+
+    slides.forEach((slides)=>{
+        slides.classList.remove("active") 
+    });
+
+    contents.forEach((content)=>{
+        content.classList.remove("active") 
+    });
+
+   btns[manual].classList.add("active");
+   slides[manual].classList.add("active");
+   contents[manual].classList.add("active");
+}
+
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        slideNav(i) 
+    })
+})
+
+// destination image slider 
+
+$('.slider').each(function() {
+    var $this = $(this);
+    var $group = $this.find('.slide_group');
+    var $slides = $this.find('.slide');
+    var bulletArray = [];
+    var currentIndex = 0;
+    var timeout;
+    
+    function move(newIndex) {
+      var animateLeft, slideLeft;
+      
+      advance();
+      
+      if ($group.is(':animated') || currentIndex === newIndex) {
+        return;
+      }
+      
+      bulletArray[currentIndex].removeClass('active');
+      bulletArray[newIndex].addClass('active');
+      
+      if (newIndex > currentIndex) {
+        slideLeft = '100%';
+        animateLeft = '-100%';
+      } else {
+        slideLeft = '-100%';
+        animateLeft = '100%';
+      }
+      
+      $slides.eq(newIndex).css({
+        display: 'block',
+        left: slideLeft
+      });
+      $group.animate({
+        left: animateLeft
+      }, function() {
+        $slides.eq(currentIndex).css({
+          display: 'none'
+        });
+        $slides.eq(newIndex).css({
+          left: 0
+        });
+        $group.css({
+          left: 0
+        });
+        currentIndex = newIndex;
+      });
+    }
+    
+    function advance() {
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        if (currentIndex < ($slides.length - 1)) {
+          move(currentIndex + 1);
+        } else {
+          move(0);
+        }
+      }, 4000);
+    }
+    
+    $('.next_btn').on('click', function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    });
+    
+    $('.previous_btn').on('click', function() {
+      if (currentIndex !== 0) {
+        move(currentIndex - 1);
+      } else {
+        move(3);
+      }
+    });
+    
+    $.each($slides, function(index) {
+      var $button = $('<a class="slide_btn">&bull;</a>');
+      
+      if (index === currentIndex) {
+        $button.addClass('active');
+      }
+      $button.on('click', function() {
+        move(index);
+      }).appendTo('.slide_buttons');
+      bulletArray.push($button);
+    });
+    
+    advance();
+  });
+
